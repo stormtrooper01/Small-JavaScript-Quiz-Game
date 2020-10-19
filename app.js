84d2dfd254a87cf891e2ae7b7fbdb6abf9a65a55
Yelena <lena.skripnik.01@gmail.com>
2020-10-19 13:45:06 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("s" + i);
+            element.innerHTML = choices[i];
+            guess("b" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
