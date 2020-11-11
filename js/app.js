44a463a796619d49201996779a1e06bfbf61785b
Yelena <lena.skripnik.01@gmail.com>
2020-11-11 17:41:40 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -39,3 +39,15 @@ function showScores() {
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
+
+var questions = [
+    new Question("Which city is the capital of Canada?", ["Toronto", "Montreal", "Ottawa", "Vancouver"], "Ottawa")
+    , new Question("What colour was Qui-Gon Jinn’s lightsaber?", ["Red", "Green", "Blue", "Violet"], "Green")
+    , new Question("What is the name of Thor’s hammer?", ["Vanir", "Norn", "Aesir", "Mjolnir"], "Mjolnir")
+    , new Question("What is the name of Flash?", ["Ben Allen", "Barry Allen", "Harry Allen", "Henry Alien"], "Barry Allen")
+    , new Question("What is Harry Potter's middle name?", ["James", "John", "Mark", "Jack"], "James")
+];
+// create quiz
+var quiz = new Quiz(questions);
+// display quiz
+populate();
