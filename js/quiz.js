07ad14b28004dff2018a3dcdff9b804fe11ae2f4
Yelena <lena.skripnik.01@gmail.com>
2020-12-01 14:29:12 +0200

    Create quiz.js

---
+++ js/quiz.js
@@ -0,0 +1,17 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
